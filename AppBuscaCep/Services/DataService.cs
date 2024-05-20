5645bc57e30b4432090fcebea018c581d58b7832
Usuario <Usuario@CPD03-M11>
2024-05-20 12:30:41 -0300

    programar service

---
+++ AppBuscaCep/Services/DataService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using AppBuscaCep.Models;
+using Java.Net;
+
+namespace AppBuscaCep.Services
+{
+    public class DataService
+    {
+        /**
+        * https://stackoverflow.com/questions/43115536/php-built-in-webserver-not-reacheable-with-ipport-via-network-cable
+        */
+
+
+        public static async Task<Endereco> GetEnderecoByCep(string cep)
+        {
+            string url = "https://cep.metoda.com.br/endereco/by-cep?cep=" + cep;
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                end = JsonConverter.DeserializeObject<Endereco>(json);
+            }
+            else
+                throw new Exception(response.RequestMessage.Content.ToString());
+        }
+
+        return end;
+    }
+
+    /**
+     * 
+     */
+
+    public static async Task<List<Bairro>> GetBairrosByCidade(int id_cidade)
+    {
+        List<Bairro> arr_bairros = new List<Bairro>();
+
+        using (HttpClient client = new HttpClient())
+        {
+            string url = "https://cep.metoda.com.br/bairro/by-cidade?id_cidade=" + id_cidade;
+
+            HttpResponseMessage response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+
+                arr_bairros = JsonConverter.Deserial<List<Bairro>>(json);
+            }
+            else
+                throw new Exception(response.RequestMessage.Content.ToString());
+        }
+
+    }
+
+    }
+}
+
--- AppBuscaCep/Views/BairrosPorCidade.xaml.cs
+++ AppBuscaCep/Views/BairrosPorCidade.xaml.cs
@@ -1,20 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using AppBuscaCep.Models;
+using AppBuscaCep.Services;
 
-namespace AppBuscaCep.Views
+namespace AppBuscaCep.Views;
+
+public partial class BairrosPorCidade : ContentPage
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class BairrosPorCidade : ContentPage
+    ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+
+    ObservableCollection<Bairro> lista_bairro = new ObservableCollection<Bairro>();
+
+    public BairrosPorCidade()
     {
-        public BairrosPorCidade()
+        InitializeComponent();
+
+        pck_cidade.ItemsSource = lista_cidades;
+
+        pck_bairros.ItemsSource = lista_bairro;
+    }
+
+    private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try 
         {
-            InitializeComponent();
+            Picker disparador = sender as Picker;
+
+            string? estado_selecionado = disparador.SelectedItem as string;
+
+            List<Cidade> arr_cidades = await DataService.GetEnderecoByEstado(
+                estado_selecionado);
+
+            lista_cidades.Clear();
+
+            arr_cidades.ForEach(i => lista_cidades.Add(i));
+        }
+        catch (Exception ex) 
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
+
+    private async void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        try 
+        {
+            Picker disparador = sender as Picker;
+
+            Cidade cidade_selecionada = disparador.SelectedItem as Cidade;
+
+            List<Bairro> arr_bairros = await DataService.GetEnderecoByCidade(
+                cidade_selecionada.id_cidade);
+
+            arr_bairros.ForEach(i => lista_bairro.Add(i));
+        }
+         catch(Exception ex) 
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
         }
     }
 }
